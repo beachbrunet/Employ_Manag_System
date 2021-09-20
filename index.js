b55bc1c76b708a6be582adeb327a4c6227797b20
Catherine Cox <87046645+beachbrunet@users.noreply.github.com>
2021-09-20 10:01:35 -0600

    started psuedo code, added gitignore, and edited readme with requirements and such

---
+++ index.js
@@ -0,0 +1,10 @@
+// MVP
+
+// Needs
+// mySQL2 package - npm i mysql2
+
+// Important: You will be committing a file that contains your database credentials.
+// Make sure that your MySQL password is not used for any other personal accounts,
+// because it will be visible on GitHub. In upcoming lessons,
+//  you will learn how to better secure this password,
+//  or you can start researching npm packages now that could help you.
