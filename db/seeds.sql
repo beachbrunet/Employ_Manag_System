dbe08769eb9e9903243e8ae2819477e913115883
Catherine Cox <87046645+beachbrunet@users.noreply.github.com>
2021-09-21 14:24:57 -0600

    setting up seed data

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,17 +1,42 @@
 -- Need values
--- id, and department name
+-- depart: id and name varchar(30)
+-- role: id, title varchar(30), salary DECIMAL, depart-id.
+-- employee: id, first_name varchar(30), LAST_NAME varchar(30), role_id INT, manager_id INT
 INSERT INTO
-  blah (Name)
+  department (id, name)
 VALUES
   (" blah blah"),
   (" blah blah"),
+  (" blah blah"),
+  (" blah blah"),
+  (" blah blah"),
+  -- table 2
 INSERT INTO
-  course_names (name)
+  role (title, salary, department_id)
 VALUES
-  ("Intro to JavaScript"),
-  ("Data Science"),
-  ("Linear Algebra"),
-  ("History of the Internet"),
-  ("Machine Learning"),
-  ("Game Design"),
-  ("Cloud Development");
+  ("Software Developer", 100000, 1),
+  (" blah blah"),
+  -- table 3
+INSERT INTO
+  employee (id, first_name, last_name, role_id, manager_id)
+VALUES
+  (" blah blah"),
+  (" blah blah"),
+  (" blah blah"),
+  (" blah blah"),
+  (" blah blah"),
+  -- example code
+  -- INSERT INTO favorite_books (book_name, section, in_stock, quantity)
+  -- VALUES ("The Great Gatsby", 1, true, 11),
+  --        ("Huckleberry Finn", 1, true, 4),
+  --        ("100 Years of Solitude", 3, false, 0),
+  --        ("Things Fall Apart", 4 , false, 0 ),
+  --        ("Crime and Punishment", 1, true, 15),
+  --        ("Moby Dick", 2, true, 11),
+  --        ("Decameron", 5, false, 0),
+  --        ("The Wind and the Willows", 2, true, 11),
+  --        ("Ulysses", 3, true, 8),
+  --        ("Madame Bovary", 4 , true, 9 ),
+  --        ("Catcher in the Rye", 1, true, 23),
+  --        ("Catch-12", 5, true, 18),
+  --        ("Hamlet", 5, true, 14);
