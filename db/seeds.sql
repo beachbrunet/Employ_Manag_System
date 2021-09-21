6f1939866e22ab3e502480cead2271c944e5ec7b
Catherine Cox <87046645+beachbrunet@users.noreply.github.com>
2021-09-21 14:05:35 -0600

    broiler plate code

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,1 +1,17 @@
 -- Need values
+-- id, and department name
+INSERT INTO
+  blah (Name)
+VALUES
+  (" blah blah"),
+  (" blah blah"),
+INSERT INTO
+  course_names (name)
+VALUES
+  ("Intro to JavaScript"),
+  ("Data Science"),
+  ("Linear Algebra"),
+  ("History of the Internet"),
+  ("Machine Learning"),
+  ("Game Design"),
+  ("Cloud Development");
