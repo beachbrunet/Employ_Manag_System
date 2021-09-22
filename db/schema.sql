f15b581e175351f15fa1d606fe20bbb7e0452be2
Catherine Cox <87046645+beachbrunet@users.noreply.github.com>
2021-09-21 19:30:09 -0600

    working on db

--- db/schema.sql
+++ db/schema.sql
@@ -1,11 +1,4 @@
 -- Outline code
-DROP DATABASE IF EXISTS employee_db;
-create DATABASE employee_db;
-USE employee_db;
--- need 3 tables roles, employees, department, role, employment
-CREATE Table department(VARCHAR(30) NOT NULL);
-CREATE Table role (VARCHAR(30) NOT NULL);
-CREATE Table employee (VARCHAR(30) NOT NULL);
 -- Example code
 -- CREATE TABLE favorite_books (
 --   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
@@ -30,3 +23,44 @@ CREATE Table employee (VARCHAR(30) NOT NULL);
 -- role_id: INT to hold reference to employee role
 -- manager_id: INT to hold reference to another employee that
 --  is the manager of the current employee (null if the employee has no manager)
+DROP DATABASE IF EXISTS employee_db;
+create DATABASE employee_db;
+USE employee_db;
+-- need 3 tables roles, employees, department, role, employment
+--
+--
+--
+--
+CREATE Table department(
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(30) NOT NULL
+);
+--
+--
+--
+--need decminal
+CREATE Table role (
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  title VARCHAR(30) NOT NULL,
+  salary VARCHAR(30) NOT NULL,
+  department_id VARCHAR(30) NOT NULL,
+  FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE
+  SET
+    NULL
+);
+--
+--
+--
+--
+--
+CREATE Table employee (
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL,
+  role_id INTEGER,
+  manager_id name VARCHAR(30) NOT NULL,
+  department INT,
+  FOREIGN KEY (role_id) REFERENCES role (id),
+  manager_id INTEGER,
+  FOREIGN KEY (manager_id) REFERENCES employee (id)
+);
