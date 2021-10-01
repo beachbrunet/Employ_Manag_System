8d449f88a022d0145c5cc552b3b7c593c853aae5
Catherine Cox <87046645+beachbrunet@users.noreply.github.com>
2021-09-30 21:31:43 -0600

    update

--- configure/connection.js
+++ configure/connection.js
@@ -4,7 +4,14 @@ let connection = mysql.createConnection({
   host: "localhost",
   port: "3301",
   user: "root",
-  password: "",
-  database: "",
+  password: "Iloveaxle1",
+  database: "employee_db",
 });
+
+// extra
+connection.connect(function (err) {
+  if (err) throw err;
+  console.log("You are Connected");
+});
+
 modules.exports = connection;
